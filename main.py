e98c2dd15d23b0a5378a274350c15e23ce9b8b6f
Evgeny Egorov <EAEgorov42@gmail.com>
2024-02-08 07:06:15 +0500

    Задание 1. Создали классы Category и Product

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+class Category:
+    name: str
+    description: str
+    products: str
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
