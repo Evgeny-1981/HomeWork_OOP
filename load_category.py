6811cb332acc7ff00e98f40e62e9c103a8deb4fa
Evgeny Egorov <EAEgorov42@gmail.com>
2024-02-09 07:15:41 +0500

    Задание 1. Создали классы Category и Product

--- load_category.py
+++ load_category.py
@@ -7,10 +7,17 @@ from main import Category, Product
 path = pathlib.Path("data/products.json")
 with open(path, 'r', encoding='utf8') as f:
     data = json.load(f)
+    list_cat = []
+    list_prod = []
 
-    for category in data:
-        category = Category(category['name'], category['description'], category['products'])
-        print(Category)
+    for item in data:
+        category = Category(item['name'], item['description'], item['products'])
+        list_cat.append(category)
+    print(list_cat)
     print(Category.quantity_category)
-    # for product in data:
+    for item in data:
+        product = Product(item['name'], ['description'], ['price'], ['quantity'])
+        list_prod.append(product)
+    print(list_prod)
+
 
