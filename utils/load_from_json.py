261e6487b3ef35318751ac5e04e4ed75d00bee00
Evgeny Egorov <EAEgorov42@gmail.com>
2024-02-12 08:03:53 +0500

    Внес рекомендации наставника

--- utils/load_from_json.py
+++ utils/load_from_json.py
@@ -3,16 +3,23 @@ from pathlib import Path
 
 from src.classes import Category, Product
 
-BASE_DIR = Path(__file__).parent
-FILE_NAME = Path(BASE_DIR, "products.json")
-with open(FILE_NAME, 'r', encoding='utf8') as f:
-    data = json.load(f)
-    category_list = []
-    product_list = []
+# BASE_DIR = Path(__file__).parent
+# FILE_NAME = Path(BASE_DIR, "products.json")
+# with open(FILE_NAME, 'r', encoding='utf8') as f:
+#     data = json.load(f)
+#     category_list = []
+#     product_list = []
 
 
 def load_categorys():
     """Загрузка категорий из файла"""
+    BASE_DIR = Path(__file__).parent
+    FILE_NAME = Path(BASE_DIR, "products.json")
+    with open(FILE_NAME, 'r', encoding='utf8') as f:
+        data = json.load(f)
+        category_list = []
+        product_list = []
+
     for item in data:
         category = Category('name',
                             'description',
@@ -23,6 +30,13 @@ def load_categorys():
 
 def load_products():
     """Загрузка продуктов из файла"""
+    BASE_DIR = Path(__file__).parent
+    FILE_NAME = Path(BASE_DIR, "products.json")
+    with open(FILE_NAME, 'r', encoding='utf8') as f:
+        data = json.load(f)
+        category_list = []
+        product_list = []
+
     for item in data:
         for prod in item['products']:
             product = Product('name',
