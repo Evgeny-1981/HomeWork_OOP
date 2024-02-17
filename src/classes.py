51cc28231f8c3481848e6dfedf9db4462ae443fd
Evgeny Egorov <EAEgorov42@gmail.com>
2024-02-17 16:29:55 +0500

    initial

--- src/classes.py
+++ src/classes.py
@@ -18,8 +18,6 @@ class Category:
         Category.quantity_category += 1
         Category.quantity_uniq_product += len(self.__products)
 
-
-
     def add_product(self, product):
         """Метод создает новый товар и вовращает объект, который можно добавить в список"""
         self.__products.append(product)
@@ -29,10 +27,19 @@ class Category:
         """Для атрибута класса Category «товары» создаем геттер, который будет выводить список товаров в формате:
         Продукт, XX руб. Остаток: XX шт."""
         for product in self.__products:
-            return [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт." for product in self.__products]
+            return [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт." for product in
+                    self.__products]
+
+    def __len__(self):
+        """Подсчет общего числа продуктов на складе"""
+        self.length = len(self.__products)
+        return self.length
 
     def __str__(self):
-        return f"{self.name}, количество продуктов: {Category.quantity_uniq_product} шт."
+        """Добавляем строковое отображение в следующем виде:
+        Название категории, количество продуктов: XX шт."""
+        return f"{self.name}, количество продуктов: {self.length} шт."
+
 
 class Product:
     """Создаем класс Product"""
@@ -49,7 +56,16 @@ class Product:
         self.quantity_in_stock = quantity_in_stock
 
     def __str__(self):
+        """ Создаем строковое отображение в следующем виде: Название продукта, ХХ руб. Остаток: ХХ шт."""
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт."
+
+    def __add__(self, other):
+        """Складваем объекты между собой.
+        Результат это сумма произведений стоимости на количество товара на складе"""
+        a = self.price * self.quantity_in_stock
+        b = other.price * other.quantity_in_stock
+        total = a + b
+        return total
 
     @classmethod
     def create_product(cls, name, description, price, quantity_in_stock):
