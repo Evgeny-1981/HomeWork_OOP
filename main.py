ce495090722b7903b7d7549a23e6563842f23713
Evgeny Egorov <EAEgorov42@gmail.com>
2024-02-28 15:46:06 +0500

    Добавил в AbstractProduct(ABC) абстрактный метод create_product(self)

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ s1 = SmartPhone('Смартфон', 'Новейший', 20000, 2, 'blue', 2, 'Re
 g1 = GrassLawn('Трава', 'Зеленая', 1000, 2, 'Green', 'RUS', 3)
 cat1 = Category('TV', 'Новые телевизоры с технологией 3D', [p1])
 
-p3 = Product.create_product('Телевизор', 'швейная', 1500, 3, 'black')
+p2 = Product.create_product('Телевизор', 'швейная', 1500, 3, 'black')
 
-sum = p1.__add__(p3)
-print(sum)
+
