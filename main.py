25f7777c19fb4e2f94ed43c252a97d82b804d00a
Evgeny Egorov <EAEgorov42@gmail.com>
2024-03-04 15:06:37 +0500

    Задание 16.1 добавил raise ValueError

---
+++ main.py
@@ -0,0 +1,20 @@
+from src.classes import Product, SmartPhone, GrassLawn, Category
+
+cat1 = Category('TV', 'Новые телевизоры с технологией 3D', [])
+p1 = Product('Телевизор', 'LG', 10000, 2, 'red')
+
+cat1.add_product(p1)
+print(cat1.__len__())
+
+p2 = Product('Телевизор', 'Philips', 15000, 2, 'red')
+cat1.add_product(p2)
+print(cat1.__len__())
+
+p3 = Product('Телевизор', 'Sony', 18000, 0, 'red')
+cat1.add_product(p3)
+print(cat1.__len__())
+
+# s1 = SmartPhone('Смартфон', 'Новейший', 20000, 2, 'blue', 2, 'Realme', 128)
+# g1 = GrassLawn('Трава', 'Зеленая', 1000, 2, 'Green', 'RUS', 3)
+#
+# p2 = Product.create_product('Телевизор', 'швейная', 1500, 3, 'black')
--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,33 @@
-class Category:
+from abc import ABC, abstractmethod
+
+
+class AbstractCategory(ABC):
+    """Создаем абстрактный класс для класса Category"""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+
+class Mixin:
+    """Создаем класс для вывода информации о созданном объекте"""
+
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self):
+        return f'Создан объект класса {self.__class__.__name__} с атрибутами: {self.__dict__}'
+
+
+class Category(Mixin, AbstractCategory):
     """Создаем класс Category!"""
     name: str
     description: str
@@ -14,61 +43,94 @@ class Category:
         self.name = name
         self.description = description
         self.__products = products
+        super().__init__()
 
         Category.quantity_category += 1
         Category.quantity_uniq_product += len(self.__products)
 
     def add_product(self, product):
-        """Метод создает новый товар и вовращает объект, который можно добавить в список"""
-        self.__products.append(product)
+        """Метод принимает на вход объект товара и добавляет его в список"""
+        if product.quantity_in_stock == 0:
+            raise  ValueError('Товар с нулевым количеством не может быть добавлен')
+        if not isinstance(product, Product):
+            raise TypeError('Нельзя добавлять в список объекты, кроме объектов Product и его наследников')
+        else:
+            self.__products.append(product)
 
     @property
     def output_products_in_category(self):
         """Для атрибута класса Category «товары» создаем геттер, который будет выводить список товаров в формате:
         Продукт, XX руб. Остаток: XX шт."""
-        for product in self.__products:
-            return [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт." for product in
-                    self.__products]
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт."
+                for product in self.__products]
 
     def __len__(self):
         """Подсчет общего числа продуктов на складе"""
-        self.length = len(self.__products)
-        return self.length
+        return len(self.__products)
 
     def __str__(self):
         """Добавляем строковое отображение в следующем виде:
         Название категории, количество продуктов: XX шт."""
-        return f"{self.name}, количество продуктов: {self.length} шт."
+        return f"{self.name}, количество продуктов: {len(self)} шт."
+
+    def __repr__(self):
+        return super().__repr__()
 
 
-class Product:
+class AbstractProduct(ABC):
+    """Создаем абстрактный класс для классов Product, SmartPhone, GrassLawn"""
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def create_product(cls, name, description, price, quantity_in_stock, color):
+        pass
+
+
+class Product(Mixin, AbstractProduct):
     """Создаем класс Product"""
     name: str
     description: str
     price: float
     quantity_in_stock: int
+    color: str
 
-    def __init__(self, name, description, price, quantity_in_stock):
+    def __init__(self, name, description, price, quantity_in_stock, color):
         """Инииализируем объект класса Product"""
         self.name = name
         self.description = description
         self.price = price
         self.quantity_in_stock = quantity_in_stock
+        self.color = color
+        super().__init__()
+
+    def __add__(self, other):
+        """Складваем объекты между собой.
+        Результат это сумма произведений стоимости на количество товара на складе"""
+        if type(self) is not type(other):
+            raise TypeError('Нельзя складывать товары разных типов')
+        total = self.price * self.quantity_in_stock + other.price * other.quantity_in_stock
+        return total
 
     def __str__(self):
         """ Создаем строковое отображение в следующем виде: Название продукта, ХХ руб. Остаток: ХХ шт."""
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт."
 
-    def __add__(self, other):
-        """Складваем объекты между собой.
-        Результат это сумма произведений стоимости на количество товара на складе"""
-        total = self.price * self.quantity_in_stock + other.price * other.quantity_in_stock
-        return total
-
     @classmethod
-    def create_product(cls, name, description, price, quantity_in_stock):
+    def create_product(cls, name, description, price, quantity_in_stock, color):
         """Метод создает новый товар и вовращает объект, который можно добавить в список"""
-        product = cls(name, description, price, quantity_in_stock)
+        product = cls(name, description, price, quantity_in_stock, color)
         return product
 
     @property
@@ -88,3 +150,30 @@ class Product:
                 self.price = new_price
         else:
             self.price = new_price
+
+    def __repr__(self):
+        return super().__repr__()
+
+
+class SmartPhone(Product):
+    """Создание дочернего класса 'Смартофон'"""
+    efficiency: float  # производитеьность
+    model: str  # модель
+    volume_ram: int  # объем встроенной памяти
+
+    def __init__(self, name, description, price, quantity_in_stock, color, efficiency, model, volume_ram):
+        super().__init__(name, description, price, quantity_in_stock, color)
+        self.efficiency = efficiency
+        self.model = model
+        self.volume_ram = volume_ram
+
+
+class GrassLawn(Product):
+    """Создание дочернего класса 'Трава газонная'"""
+    country_origin: str  # страна-производитель
+    germ_period: int  # срок прорастания
+
+    def __init__(self, name, description, price, quantity_in_stock, color, country_origin, germ_period):
+        super().__init__(name, description, price, quantity_in_stock, color)
+        self.country_origin = country_origin
+        self.germ_period = germ_period
